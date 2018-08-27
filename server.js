aafe9e6441179f1d32fb5c878288d79934892cb7
leanfj <leanfj@hotmail.com>
2018-08-27 10:38:07 -0300

    update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const https = require('https');
 var router = express.Router();
@@ -27,6 +28,7 @@ const rotaBing = (router.get = ('/',
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', rotaBing);
 
