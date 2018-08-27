b0302779b4fae1892ce999c7c2bbed439e042e06
leanfj <leanfj@hotmail.com>
2018-08-27 10:23:25 -0300

    push inicial

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+var router = express.Router();
+
+const rotaBing = (router.get = ('/',
+(reqest, response) => {
+  let resultado = '';
+
+  let req = https.get(
+    'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR',
+    resp => {
+      resp.on('data', data => {
+        resultado += data;
+      });
+      resp.on('end', data => {
+        response.json(JSON.parse(resultado));
+      });
+    }
+  );
+
+  req.on('error', data => {
+    console.dir(data);
+    response.json(resultado);
+  });
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', rotaBing);
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log('App escutando porta 3000');
+});
